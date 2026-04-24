e18f6bcf228d3711f5a23f76c2b4786509371323
FERNANDO AUGUSTO CAMUSSI <24271866@cfp107_2.local>
2026-04-24 16:49:30 -0300

    aula 01

--- app/index.tsx
+++ app/index.tsx
@@ -8,8 +8,34 @@ import {
   Platform,
 } from "react-native";
 import { styles } from "./style";
+import { useState } from "react";
 
 export default function Index() {
+  const [input, setInput] = useState("");
+  // Estado que armazena todas as mensagens da conversa
+  const [message, setMessage] = useState<{ role: string; content: string }[]>(
+    [],
+  );
+
+  // Função executada ao clicar no botão
+  async function sendMessage() {
+    // Impede envio de mensagem vazia
+    if (!input.trim()) return;
+
+    // Cria objeto da mensagem do usuário
+    const userMessage = {
+      role: "user",
+      content: input,
+    };
+
+    // Adiciona mensagem do usuário ao chat
+    setMessage((prev) => [...prev, userMessage]); 
+    // "..." representa os "spread operator" -> ele "espalha" os itens do array dentro de outro array
+
+    // Limpa o campo de texto:
+    setInput("");
+  }
+
   return (
     <KeyboardAvoidingView
       style={styles.keyboard}
